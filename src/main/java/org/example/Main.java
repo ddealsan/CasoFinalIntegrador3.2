3fee6b13768562ed9b098f705708c25b3062d48c
ddealsan <ddealsan@myuax.com>
2024-03-20 00:17:53 +0100

    finalizamos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -28,7 +28,7 @@ public class Main {
         interfazUsuario.mostrarMenu();
         int opcion = interfazUsuario.leerOpcion();
 
-        // Realizar acción según la opción seleccionada
+        // Realizar acción según la opción seleccionad
         if (opcion == 1) {
             // Configurar simulación
             Autenticacion autenticacion = new Autenticacion();
