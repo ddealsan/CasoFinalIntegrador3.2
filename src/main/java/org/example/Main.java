1d6568afac68fa6eeea7bf0ea61d10e0cebe0419
ddealsan <ddealsan@myuax.com>
2024-03-19 22:55:30 +0100

    avanzamos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,15 @@
 package org.example;
 
+import Entidades.Animal;
+import Entidades.Planta;
+import Entidades.Ambiente;
+import Poblaciones.SimuladorEcosistema;
+import gestion.InterfazUsuario;
+import gestion.Autenticacion;
+import gestion.RegistroActividades;
+import analisis_avanzado.ResolucionProblemas;
+import analisis_avanzado.VisualizacionDatos;
+import analisis_avanzado.IntegracionNuevasFunciones;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
