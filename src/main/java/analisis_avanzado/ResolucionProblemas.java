0e4141c4aa2699f6dede7fbde3763e636f9264df
ddealsan <ddealsan@myuax.com>
2024-03-19 22:50:26 +0100

    avanzamos

--- src/main/java/analisis_avanzado/ResolucionProblemas.java
+++ src/main/java/analisis_avanzado/ResolucionProblemas.java
@@ -1,4 +1,13 @@
 package analisis_avanzado;
 
 public class ResolucionProblemas {
+    public void buscarEquilibriosEcologicos() {
+        // Implementación para buscar equilibrios ecológicos
+    }
+
+    public void evaluarEstrategiasConservacion() {
+        // Implementación para evaluar estrategias de conservación
+    }
+
+    // Otros métodos para resolver problemas específicos
 }
