218eeee5704e375cf000611b574bd054598a7561
ddealsan <ddealsan@myuax.com>
2024-03-19 22:51:46 +0100

    avanzamos

--- src/main/java/analisis_avanzado/VisualizacionDatos.java
+++ src/main/java/analisis_avanzado/VisualizacionDatos.java
@@ -1,4 +1,15 @@
 package analisis_avanzado;
 
 public class VisualizacionDatos {
+    public void visualizarDatosSimulacion(double[] datos) {
+        // Implementación para visualizar los datos de la simulación
+        System.out.println("Visualizando datos de la simulación:");
+
+        // Aquí podrías implementar la lógica para generar gráficos, diagramas, o cualquier otra representación visual de los datos
+        for (int i = 0; i < datos.length; i++) {
+            System.out.println("Dato " + (i + 1) + ": " + datos[i]);
+        }
+    }
+
+
 }
