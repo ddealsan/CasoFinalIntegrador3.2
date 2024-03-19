e8ec5dcbff7389d52f4a4ea0ba306f5de6a30f4a
ddealsan <ddealsan@myuax.com>
2024-03-19 15:01:34 +0100

    avanzamos

---
+++ src/main/java/Entidades/Ambiente.java
@@ -0,0 +1,23 @@
+package Entidades;
+
+public class Ambiente {
+    private String clima;
+    private String terreno;
+    private int recursosDisponibles;
+
+    public Ambiente(String clima, String terreno, int recursosDisponibles) {
+        this.clima = clima;
+        this.terreno = terreno;
+        this.recursosDisponibles = recursosDisponibles;
+    }
+
+    public void cambiarClima(String nuevoClima) {
+        this.clima = nuevoClima;
+    }
+
+    public void cambiarRecursosDisponibles(int nuevosRecursos) {
+        this.recursosDisponibles = nuevosRecursos;
+    }
+
+    // Otros métodos y getters/setters según sea necesario
+}
